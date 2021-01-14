78a13a2fd6e1a7df065675e9681827c399d4d461
DarthHamza <hamzamakia@gmail.com>
2021-01-14 09:53:10 +0300

    deleted some comments

--- nodes.js
+++ nodes.js
@@ -9,8 +9,6 @@ const node = new Node("Laila");
 const node2 = new Node("Mohammad", node);
 const node3 = new Node("Hasan", node);
 
-// console.log(`Node data: ${node.data}, connected to: ${node.link}`);
-// console.log(`Node 2 data: ${node2.data}, connected to: ${node2.link.data}`);
 console.log(node3.data);
 console.log(node3.link.data);
 console.log(node3.link.link);
